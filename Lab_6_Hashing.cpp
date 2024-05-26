ee4a3002b035a9419943451d8de39fa7734025b2
Letaga <Inventor2004nn@gmail.com>
2024-05-26 17:16:38 +0300

    Добавьте файлы проекта.

---
+++ Lab_6_Hashing.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+const int BucketsSize = 7;
+
+int hashKey(int key)
+{
+	if (key < 0)
+		return -key;
+	return key;
+}
+int hashKey(const string& key)
+{
+	int hash = 0;
+	for (char c : key)
+		hash += c;
+	return hash;
+}
+int hashKey(char key)
+{
+	return (int)key;
+}
+
+
+template<class T, class K>
+class Hashing
+{
+public:
+
+	void insert(T key, K value)
+	{
+		int hash = hashKey(key);
+		if(Buckets[hash % BucketsSize].insert({ key,value }).second)
+			sizeB++;
+	}
+
+	K& at(const T& key)
+	{
+		int hash = hashKey(key);
+		return Buckets[hash % BucketsSize].at(key);
+	}
+
+	K& operator[](const T& key)
+	{
+		int hash = hashKey(key);
+		return Buckets[hash % BucketsSize][key];
+	}
+
+	void erase(const T& key)
+	{
+		int hash = hashKey(key);
+		sizeB -= Buckets[hash % BucketsSize].erase(key);
+	}
+
+	void clear()
+	{
+		for (auto& m : Buckets)
+			m.clear();
+		sizeB = 0;
+	}
+
+	void show()
+	{
+		cout << ">" << endl;
+		for (const auto& m : Buckets)
+		{
+			for (const auto& p : m)
+				cout << "{ " << p.first << ", " << p.second << " }; ";
+			cout << endl;
+		}
+		cout << "<" << endl;
+	}
+
+	int size()
+	{
+		return sizeB;
+	}
+
+private:
+	map<T, K> Buckets[BucketsSize] = {};
+	int sizeB = 0;
+};
+
+void testInt()
+{
+	Hashing <int, int> h;
+	cout << "int, int" << endl;
+
+	h.insert(5, 8);
+	h.insert(32, 7);
+	h.insert(8, 4);
+	h.insert(6, 30);
+	h[5] = 3;
+	h.show();
+
+	h.erase(5);
+	h.show();
+
+	h.clear();
+	h.show();
+}
+
+void testStr()
+{
+	Hashing <string, string> tea;
+	cout << "\n===========================\nstring, string" << endl;
+	tea.insert("Bai Hao Yinzhen", "white tea");
+	tea.insert("Jasmine", "green tea");
+	tea.insert("Earl grey", "black tea");
+	tea.insert("Karkade", "red tea");
+	tea.show();
+	cout << "Jasmine is " << tea.at("Jasmine");
+}
+
+int main()
+{
+	testInt();
+	testStr();	
+}
